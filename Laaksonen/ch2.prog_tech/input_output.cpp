258e276813fe9c071412d558c310f4cf2a9ae46d
Zihao WANG <wangzihao820@hotmail.com>
2022-01-11 11:26:14 +0000

    Begin with Laaksonen

---
+++ Laaksonen/ch2.prog_tech/input_output.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+  // these two lines makes the io faster
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int a, b;
+  string x;
+  /* a demo for stdin
+  cin >> a >> b >> x;
+  */
+  a = 123, b = 456, x = "monkey";
+  cout << a << " " << b << " " << x << "\n";
+
+  /* re direct files as input and output
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  */
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# competitive_programming
+# Competitive Programming in C++
+
+## 1. The book by Laaksonen
+- [ ] CH2 Programming Techniques
+
+## 2. Chapters in USACO
+- [ ] 
+
+## 3. Leetcode Weekly Contest
+...
